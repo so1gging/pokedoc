606268631c45676645e906c54165c2431a77927c
김솔지 <so1gging@gmail.com>
2023-03-26 20:20:05 +0900

    svg setting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      oneOf: [
+        {
+          loader: '@svgr/webpack',
+          resourceQuery: { not: [/url/] },
+          options: {
+            // SVG 파일 컴파일 시 viewBox를 포함하도록 설정
+            dimensions: false,
+          },
+        },
+        {
+          type: 'asset',
+          resourceQuery: /url/, // svg 끝에 ?url 을 붙으면 asset 으로 모듈로 번들링합니다.
+        },
+      ],
+    })
+
+    return config
+  },
 }
 
 module.exports = nextConfig

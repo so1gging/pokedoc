4f35a226f30c0cb15016a2106b2bbcadfa3ea1c5
김솔지 <so1gging@gmail.com>
2023-03-26 21:06:47 +0900

    Chip 추가

---
+++ components/Chip/Chip.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Chip = styled.span`
+  display: inline-block;
+  padding: 2px 8px;
+  color: ${(props) => props.theme.white};
+  font-size: 10px;
+  line-height: 16px;
+  font-weight: 700;
+  border-radius: 10px;
+`

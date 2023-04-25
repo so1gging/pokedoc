3ec8acba6f385a6193603814222e14bd1f56e1b4
김솔지 <so1gging@gmail.com>
2023-04-25 21:24:09 +0900

    typography style 추가

--- libs/theme/index.ts
+++ libs/theme/index.ts
@@ -2,10 +2,46 @@ import { graph_colors, main_colors, pokemon_type_colors } from '~/libs/theme/col
 
 export const fontFamily = `'Pretendard', sans-serif`
 
+const fontSize = {
+  head: '24px',
+  subtitle1: '14px',
+  subtitle2: '12px',
+  subtitle3: '10px',
+  body1: '14px',
+  body2: '12px',
+  body3: '10px',
+  caption: '8px',
+}
+
+const fontWeight = {
+  head: 'bold',
+  subtitle1: 'bold',
+  subtitle2: 'bold',
+  subtitle3: 'bold',
+  body1: 'regular',
+  body2: 'regular',
+  body3: 'regular',
+  caption: 'regular',
+}
+
+const lineHeight = {
+  head: '32px',
+  subtitle1: '16px',
+  subtitle2: '16px',
+  subtitle3: '16px',
+  body1: '16px',
+  body2: '16px',
+  body3: '16px',
+  caption: '12px',
+}
+
 const theme = {
-  ...main_colors,
-  ...pokemon_type_colors,
-  ...graph_colors,
+  main_colors,
+  pokemon_type_colors,
+  graph_colors,
+  fontSize,
+  fontWeight,
+  lineHeight,
 }
 
 export default theme

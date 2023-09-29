21a451a96dc969a805027255be06cb5b6b831d7e
김솔지 <so1gging@gmail.com>
2023-09-29 18:40:43 +0900

    :tada:

---
+++ libs/styles/theme.ts
@@ -0,0 +1,58 @@
+const theme = {
+  color: {
+    identity: {
+      primary: '#DC0A2D',
+    },
+    pokemon: {
+      Bug: '#A7B723',
+      Dark: '#75574C',
+      Dragon: '#7037FF',
+      Electric: '#F9CF30',
+      Fairy: '#E69EAC',
+      Fighting: '#C12239',
+      Fire: '#F57D31',
+      Flying: '#A891EC',
+      Ghost: '#70559B',
+      Normal: '#AAA67F',
+      Grass: '#74CB48',
+      Ground: '#DEC16B',
+      Ice: '#9AD6DF',
+      Poison: '#A43E9E',
+      Psychic: '#FB5584',
+      Rock: '#B69E31',
+      Steel: '#B7B9D0',
+      Water: '#6493EB',
+    },
+    grayscale: {
+      Dark: '#1D1D1D',
+      Medium: '#666666',
+      Light: '#E0E0E0',
+      Background: '#EFEFEF',
+      White: '#FFFFFF',
+    },
+  },
+  font: {
+    body: 'arial',
+  },
+  typography: {
+    header: {
+      headline: 'Bold 24/32px',
+      subtitle1: 'Bold 14/16px',
+      subtitle2: 'Bold 12/16px',
+      subtitle3: 'Bold 10/16px',
+    },
+    body: {
+      body1: 'Regular 14/26px',
+      body2: 'Regular 12/26px',
+      body3: 'Regular 10/26px',
+      caption: 'Regular 8/12px',
+    },
+  },
+  shadow: {
+    shadow1: '0px 1px 3px 1px rgba(0, 0, 0, 0.20)',
+    shadow2: '0px 3px 12px 3px rgba(0, 0, 0, 0.20)',
+    inner: ' 0px 1px 3px 1px rgba(0, 0, 0, 0.25) inset',
+  },
+}
+
+export default theme
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
-import type { Config } from 'tailwindcss'
+/** @type {import('tailwindcss').Config} */
 
-const config: Config = {
+module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -8,13 +8,61 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        identity: {
+          primary: '#DC0A2D',
+        },
+        pokemon: {
+          Bug: '#A7B723',
+          Dark: '#75574C',
+          Dragon: '#7037FF',
+          Electric: '#F9CF30',
+          Fairy: '#E69EAC',
+          Fighting: '#C12239',
+          Fire: '#F57D31',
+          Flying: '#A891EC',
+          Ghost: '#70559B',
+          Normal: '#AAA67F',
+          Grass: '#74CB48',
+          Ground: '#DEC16B',
+          Ice: '#9AD6DF',
+          Poison: '#A43E9E',
+          Psychic: '#FB5584',
+          Rock: '#B69E31',
+          Steel: '#B7B9D0',
+          Water: '#6493EB',
+        },
+        grayscale: {
+          Dark: '#1D1D1D',
+          Medium: '#666666',
+          Light: '#E0E0E0',
+          Background: '#EFEFEF',
+          White: '#FFFFFF',
+        },
+      },
+      font: {
+        body: 'arial',
+      },
+      typography: {
+        header: {
+          headline: 'Bold 24/32px',
+          subtitle1: 'Bold 14/16px',
+          subtitle2: 'Bold 12/16px',
+          subtitle3: 'Bold 10/16px',
+        },
+        body: {
+          body1: 'Regular 14/26px',
+          body2: 'Regular 12/26px',
+          body3: 'Regular 10/26px',
+          caption: 'Regular 8/12px',
+        },
+      },
+      shadow: {
+        shadow1: '0px 1px 3px 1px rgba(0, 0, 0, 0.20)',
+        shadow2: '0px 3px 12px 3px rgba(0, 0, 0, 0.20)',
+        inner: ' 0px 1px 3px 1px rgba(0, 0, 0, 0.25) inset',
       },
     },
   },
   plugins: [],
 }
-export default config

0f07fbd255f899e2a2221c0fdd53af427f00a8f8
김솔지 <so1gging@gmail.com>
2023-09-29 19:06:09 +0900

    :fire:

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import Typography from '@/components/Typography'
+import Badge from '@/components/Badge'
+
 export default function Home() {
-  return <main className="flex"></main>
+  return (
+    <main className="text">
+      <Typography text="123" />
+      <Typography variant="caption" text="123" />
+      <Badge type="Bug" text="벌레" />
+    </main>
+  )
 }
--- components/Typography.tsx
+++
@@ -1,8 +0,0 @@
-interface TypographyProps {
-  variant?: 'headline' | 'subtitle1' | 'subtitle2' | 'subtitle3' | 'body1' | 'body2' | 'body3' | 'caption'
-  text: string | null | undefined
-}
-
-export default function Typography({ variant = 'body1', text }: TypographyProps) {
-  return <p className={`text-${variant}`}>{text}</p>
-}

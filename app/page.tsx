2ee53a4d2c42ce4bfee7eda47606526a99a7e6b7
김솔지 <so1gging@gmail.com>
2023-09-30 14:20:44 +0900

    :sparkles: page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <main className="text"></main>
+import { getPokemon } from '@/libs/apis/pokemons/apis'
+import PokemonList from '@/templates/PokemonList'
+import TitleBar from '@/templates/layout/TitleBar'
+
+export default async function Home() {
+  const dd = await getPokemon()
+
+  console.log('pokemon/123/'.match('(?<=pokemon/)(.*)(?=/)'))
+  return (
+    <main>
+      <TitleBar />
+      <PokemonList data={dd} />
+    </main>
+  )
 }
--- libs/utils/query.ts
+++ libs/utils/query.ts
@@ -1,4 +1,9 @@
 export const parseQueryParams = (obj: Record<string, any>) => {
   const keys = Object.keys(obj)
+
+  if (keys.length === 0) {
+    return undefined
+  }
+
   return keys.map((key) => `${key}=${obj[key]}`).join('&')
 }

281705c8526741e8c33eae96032f8b3202bb18f3
김솔지 <so1gging@gmail.com>
2023-10-01 17:39:08 +0900

    :lipstick: TitleBar 추가

--- templates/layout/TitleBar.tsx
+++ templates/layout/TitleBar.tsx
@@ -1,8 +1,10 @@
 import Pokeball from '@/assets/icons/pokeball.svg'
 import Logo from '@/assets/icons/logo.svg'
+import { useRouter } from 'next/navigation'
 export default function TitleBar() {
+  const router = useRouter()
   return (
-    <div className="flex gap-4 pt-3 pb-6 px-3">
+    <div className="flex gap-4 pt-3 pb-6 px-3 cursor-pointer" onClick={() => router.push('/')}>
       <Pokeball width={24} fill="white" />
       <Logo />
     </div>

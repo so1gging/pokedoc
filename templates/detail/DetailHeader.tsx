c58bcdb827728c406d9d1ab5be01754066faa794
김솔지 <so1gging@gmail.com>
2023-10-01 17:17:38 +0900

    :lipstick: pointer cursor 추가

--- templates/detail/DetailHeader.tsx
+++ templates/detail/DetailHeader.tsx
@@ -14,7 +14,7 @@ export default function DetailHeader({ id, name }: DetailHeaderProps) {
 
   return (
     <div className="w-full flex gap-2 items-center pt-5 pb-6 px-5 justify-between">
-      <Back fill="white" onClick={handleBack} />
+      <Back fill="white" onClick={handleBack} className="cursor-pointer" />
       <span className="text-headline text-white">{name}</span>
       <span className="text-subtitle2 text-white">#{id}</span>
     </div>

0a17a53fa93d156a5dbd4b87ed0ee517fd63f667
김솔지 <so1gging@gmail.com>
2023-10-01 16:42:55 +0900

    :sparkles: api request 수정

--- libs/apis/pokemons/apis.ts
+++ libs/apis/pokemons/apis.ts
@@ -2,13 +2,13 @@ import getFetcher from '@/libs/apis/fetcher'
 import { GetPokemonRequest, GetPokemonResponse } from '@/libs/apis/pokemons/models'
 
 export const getPokemonList = async (reqParams?: GetPokemonRequest) => {
-  const limit = reqParams?.limit ?? 20
+  const limit = reqParams?.limit ?? 50
   const page = reqParams?.page ?? 1
   const offset = page !== 1 ? page * limit : 0
 
   const req = {
     path: '/pokemon',
-    params: { limit: reqParams?.limit ?? 20, offset: offset },
+    params: { limit, offset },
   }
   return await getFetcher<GetPokemonResponse>(req)
 }

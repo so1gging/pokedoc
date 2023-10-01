333e06b5ed9ac016119d3f704f83663ae8b12bfb
김솔지 <so1gging@gmail.com>
2023-10-01 14:41:47 +0900

    :sparkles: species api 추가

---
+++ libs/apis/pokemon-species/models.ts
@@ -0,0 +1,39 @@
+import { CommonPokemonType } from '@/libs/apis/common/models'
+
+interface PokemonGenera {
+  genus: string
+  language: {
+    name: string
+    url: string
+  }
+}
+export interface GetPokemonSpeciesResponse {
+  id: number
+  name: string
+  order: number
+  gender_rate: number
+  capture_rate: number
+  base_happiness: number
+  is_baby: boolean
+  is_legendary: boolean
+  is_mythical: boolean
+  hatch_counter: number
+  has_gender_differences: boolean
+  forms_switchable: boolean
+  growth_rate: CommonPokemonType
+  pokedex_numbers: []
+  egg_groups: CommonPokemonType
+  color: CommonPokemonType
+  shape: CommonPokemonType
+  evolves_from_species: CommonPokemonType
+  evolution_chain: {
+    url: string
+  }
+  habitat: null
+  generation: CommonPokemonType
+  names: []
+  flavor_text_entries: []
+  form_descriptions: []
+  genera: PokemonGenera[]
+  varieties: []
+}

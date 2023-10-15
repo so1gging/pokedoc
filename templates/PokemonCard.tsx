88ee90143da958b0ffbf95c5e77483ae739eb040
김솔지 <so1gging@gmail.com>
2023-10-15 17:03:33 +0900

    :lipstick: hover시 포켓몬 이미지가 움직이도록

--- templates/PokemonCard.tsx
+++ templates/PokemonCard.tsx
@@ -1,6 +1,8 @@
 'use client'
 import Card from '@/components/Card/Card'
 import { useRouter } from 'next/navigation'
+import Image from 'next/image'
+import { useState } from 'react'
 
 interface PokemonCardProps {
   name: string
@@ -8,9 +10,11 @@ interface PokemonCardProps {
 }
 export default function PokemonCard({ name, url }: PokemonCardProps) {
   const router = useRouter()
+  const [hover, setHover] = useState(false)
 
   const number = url.match('(?<=pokemon/)(.*)(?=/)')?.[0] ?? 'no'
-  const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${number}.png`
+  const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/${number}.png`
+  const animatedImageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${number}.gif`
 
   const handleClick = () => {
     router.push(`/detail/${number}`)
@@ -18,7 +22,20 @@ export default function PokemonCard({ name, url }: PokemonCardProps) {
 
   return (
     <div className="cursor-pointer" onClick={handleClick}>
-      <Card key={name} subtitle={number} title={name} src={imageUrl} />
+      <div onMouseOver={() => setHover(true)} onMouseOut={() => setHover(false)}>
+        <Card
+          key={name}
+          subtitle={number}
+          title={name}
+          content={
+            hover ? (
+              <Image src={animatedImageUrl} width={72} height={72} alt={'pokemon'} />
+            ) : (
+              <Image src={imageUrl} width={72} height={72} alt={'pokemon'} />
+            )
+          }
+        />
+      </div>
     </div>
   )
 }

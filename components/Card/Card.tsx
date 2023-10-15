fab0b37f40572e1d0327fa40bccb45f94bd8a924
김솔지 <so1gging@gmail.com>
2023-10-15 17:03:13 +0900

    :lipstick: Card 의 컨텐츠는 props로 넘길 수 있도록

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,18 +1,16 @@
 'use client'
-import Image from 'next/image'
+import { ReactNode } from 'react'
 
 interface CardProps {
   subtitle: string
   title: string
-  src: string
+  content: ReactNode
 }
-export default function Card({ subtitle, title, src }: CardProps) {
+export default function Card({ subtitle, title, content }: CardProps) {
   return (
     <div className="flex flex-col w-104 h-108 shadow-shadow2 bg-white rounded-md relative justify-between">
       <span className="text-right text-grayscale-medium text-caption py-1 px-2">#{subtitle}</span>
-      <div className="absolute top-3 left-4">
-        <Image src={src} width={72} height={72} alt={'pokemon'} />
-      </div>
+      <div className="absolute top-3 left-4">{content}</div>
       <div className="w-full bg-grayscale-Background text-center pt-12 px-2 pb-1 rounded-b">{title}</div>
     </div>
   )

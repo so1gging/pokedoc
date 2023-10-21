14cb334f45494e7353f3376de53883e640a26d37
김솔지 <so1gging@gmail.com>
2023-10-21 21:20:03 +0900

    :test_tube: 무한스크롤링 테스트코드 추가

--- cypress/e2e/index.cy.js
+++ cypress/e2e/index.cy.js
@@ -1,5 +1,10 @@
 describe('메인페이지 테스트', () => {
   it('index 페이지 접속시, 메인페이지가 그려지는지 테스트 합니다.', function () {
     cy.visit('/')
+    cy.wait(500)
+    cy.contains('electrode').should('not.exist')
+    cy.get('[data-testid="pokemon-list"]').scrollTo('bottom')
+    cy.wait(500)
+    cy.contains('electrode').should('exist')
   })
 })

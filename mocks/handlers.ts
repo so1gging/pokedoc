5dba727bb957c71076a4da679213d60b3a3dc302
김솔지 <so1gging@gmail.com>
2023-10-21 21:19:40 +0900

    :test_tube: 페이징을 위해 handler수정

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -2,8 +2,26 @@ import { rest } from 'msw'
 import mockPokemons from '../cypress/fixtures/pokemons.json'
 
 const handler = [
-  rest.get('https://pokeapi.co/api/v2/pokemon', (_, res, ctx) => {
-    return res(ctx.json(mockPokemons))
+  rest.get('https://pokeapi.co/api/v2/pokemon', (req, res, ctx) => {
+    if (req.url.search === '?limit=50&offset=0') {
+      return res(
+        ctx.json({
+          count: 1292,
+          next: 'https://pokeapi.co/api/v2/pokemon?offset=50&limit=50',
+          previous: null,
+          results: mockPokemons.results.slice(0, 50),
+        }),
+      )
+    } else {
+      return res(
+        ctx.json({
+          count: 1292,
+          next: 'https://pokeapi.co/api/v2/pokemon?offset=50&limit=50',
+          previous: null,
+          results: mockPokemons.results.slice(50, 100),
+        }),
+      )
+    }
   }),
 ]
 

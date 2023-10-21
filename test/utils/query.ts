f24950e9b02dbd09eed121f6880294f71f1d8ee9
김솔지 <so1gging@gmail.com>
2023-10-21 19:25:19 +0900

    :goal_net:

--- test/utils/query.ts
+++ test/utils/query.ts
@@ -2,9 +2,9 @@ import { parseQueryParams } from '@/libs/utils/query'
 
 describe('Query Utils', () => {
   test('parseQueryParams', () => {
-    expect(parseQueryParams({})).toEqual('')
-    expect(parseQueryParams({ param: 123 })).toEqual('param=123')
-    expect(parseQueryParams({ param: 123, param2: 133 })).toEqual('param=123&param2=133')
-    expect(parseQueryParams({ param: 123, param2: undefined })).toEqual('param=123&param2=undefined')
+    expect(parseQueryParams({})).equal('')
+    expect(parseQueryParams({ param: 123 })).equal('param=123')
+    expect(parseQueryParams({ param: 123, param2: 133 })).equal('param=123&param2=133')
+    expect(parseQueryParams({ param: 123, param2: undefined })).equal('param=123&param2=undefined')
   })
 })

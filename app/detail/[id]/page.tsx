a190851241af6c2869cfdaceaa093a5ce004ae00
김솔지 <so1gging@gmail.com>
2023-10-21 15:30:32 +0900

    :lipstick: 상세페이지 수정

--- app/detail/[id]/page.tsx
+++ app/detail/[id]/page.tsx
@@ -1,5 +1,4 @@
 'use client'
-import TypeBadge from '@/components/TypeBadge/TypeBadge'
 import DetailHeader from '@/templates/detail/DetailHeader'
 import DetailOverview from '@/templates/detail/DetailOverview'
 import DetailImage from '@/templates/detail/DetailImage'
@@ -8,6 +7,8 @@ import DetailBaseStats from '@/templates/detail/DetailBaseStats'
 import DetailAppearance from '@/templates/detail/DetailAppearance'
 import Spinner from '@/components/Spinner/Spinner'
 import useGetPokemonAllDetails from '@/templates/detail/hooks/useGetPokemonAllDetails'
+import DetailContents from '@/templates/detail/DetailContents'
+import DetailTypes from '@/templates/detail/DetailTypes'
 
 export default function Page({ params }: { params: { id: string } }) {
   const detailData = useGetPokemonAllDetails(params.id)
@@ -27,17 +28,13 @@ export default function Page({ params }: { params: { id: string } }) {
         header={<DetailHeader name={detailData.name} id={detailData.id} />}
         pokemonImage={<DetailImage id={detailData.id} />}
       />
-      <div className="w-full flex flex-col gap-4 items-center pt-20 px-5">
-        <div className="flex gap-1">
-          {detailData.types.map((name) => (
-            <TypeBadge key={name} title={name} type={name} />
-          ))}
-        </div>
+      <DetailContents>
+        <DetailTypes types={detailData.types} />
         <DetailAbout weight={detailData.weight} height={detailData.height} abilities={detailData.abilities} />
         <span className="text-center">{detailData.flavor}</span>
         <DetailBaseStats stats={detailData.stats} />
         <DetailAppearance basic={detailData.sprites} versions={detailData.sprites.versions} />
-      </div>
+      </DetailContents>
     </>
   )
 }

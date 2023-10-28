61789157e0ac0f485b43ddc6a443cc2edd382e1a
김솔지 <so1gging@gmail.com>
2023-10-28 19:26:35 +0900

    :lipstick: Card 디자인 수정

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,17 +1,35 @@
 'use client'
 import { ReactNode } from 'react'
+import Pokeball from '@/assets/icons/pokeball.svg'
+import theme from '@/libs/styles/theme'
 
 interface CardProps {
-  subtitle: string
   title: string
   content: ReactNode
+  subTitles: string[]
+  color?: string
 }
-export default function Card({ subtitle, title, content }: CardProps) {
+export default function Card({ title, content, subTitles, color }: CardProps) {
   return (
-    <div className="flex flex-col shadow-shadow1 bg-white rounded-md relative justify-between">
-      <span className="text-right text-grayscale-medium text-caption py-1 px-2">#{subtitle}</span>
-      <div className="absolute top-1 left-1/2 -translate-x-1/2">{content}</div>
-      <div className="w-full bg-grayscale-Background text-center pt-12 px-2 pb-1 rounded-b">{title}</div>
+    <div className={`flex flex-col w-[280px] h-[200px] shadow-shadow1 rounded-[20px] ${color}`}>
+      <div className="relative w-full h-full overflow-hidden pt-[50px] pb-[10px] px-[15px]">
+        <div className="absolute -right-16 -top-11">
+          <Pokeball width={200} fill={theme.color.grayscale.Background} opacity={0.2} />
+        </div>
+        <div className="text-headline text-white">{title}</div>
+        <div className="flex flex-col gap-[10px]">
+          {subTitles.map((sub) => (
+            <div
+              key={sub}
+              className="text-subtitle1 w-fit font-medium text-white p-[8px] px-[20px] rounded-[20px]"
+              style={{ backgroundColor: 'rgba(255, 255, 255, 0.2)' }}
+            >
+              {sub}
+            </div>
+          ))}
+        </div>
+        <div className="absolute bottom-0 right-[10px]">{content}</div>
+      </div>
     </div>
   )
 }

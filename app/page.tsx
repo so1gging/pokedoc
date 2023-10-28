fd4d3c5ff1e8e6dffeea9e447343e0811c59dc4c
김솔지 <so1gging@gmail.com>
2023-10-28 19:27:30 +0900

    :lipstick: page 수정

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main>
       <Header />
-      <div className="fixed w-full h-full top-0 z-50 pt-32 pl-12 pr-12 pb-12">
+      <div className="fixed w-full h-full top-0 z-50 pt-32 pl-12 pr-12 pb-12 flex justify-center">
         <PokemonList />
       </div>
     </main>

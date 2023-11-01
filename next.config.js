f448f3506c2a8dcaa5d606a41c18289e31f81c2c
김솔지 <so1gging@gmail.com>
2023-11-01 11:38:15 +0900

    :lipstick: svg 설정 수정

--- next.config.js
+++ next.config.js
@@ -11,7 +11,20 @@ const nextConfig = {
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ['@svgr/webpack'],
+      oneOf: [
+        {
+          loader: '@svgr/webpack',
+          resourceQuery: { not: [/url/] },
+          options: {
+            // SVG 파일 컴파일 시 viewBox를 포함하도록 설정
+            dimensions: false,
+          },
+        },
+        {
+          type: 'asset',
+          resourceQuery: /url/, // svg 끝에 ?url 을 붙으면 asset 으로 모듈로 번들링합니다.
+        },
+      ],
     })
 
     return config

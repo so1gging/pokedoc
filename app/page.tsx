4f51a537cdc99366a83fa1b689c5c234bf9ce757
김솔지 <so1gging@gmail.com>
2023-12-13 16:17:52 +0900

    :necktie: react-query provider 감싸기

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,21 @@
 import HomeTemplate from '@/templates/pages/home/HomeTemplate'
 import Pokeball from '@/assets/icons/pokeball.svg'
 import theme from '@/libs/styles/theme'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient()
 
 export default function Home() {
   return (
     <main>
-      <div className="relative w-screen h-screen overflow-hidden">
-        <div className="absolute -right-16 -top-11">
-          <Pokeball width={200} fill={theme.color.grayscale.Background} />
+      <QueryClientProvider client={queryClient}>
+        <div className="relative w-screen h-screen overflow-hidden">
+          <div className="absolute -right-16 -top-11">
+            <Pokeball width={200} fill={theme.color.grayscale.Background} />
+          </div>
+          <HomeTemplate />
         </div>
-        <HomeTemplate />
-      </div>
+      </QueryClientProvider>
     </main>
   )
 }

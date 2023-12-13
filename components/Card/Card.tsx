9f49dc3696eed04a161df079b7bb569a6a5798e1
김솔지 <so1gging@gmail.com>
2023-12-13 14:49:57 +0900

    :lipstick: Card컴포넌트 수정

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,34 +1,39 @@
 'use client'
-import { ReactNode } from 'react'
 import Pokeball from '@/assets/icons/pokeball.svg'
 import theme from '@/libs/styles/theme'
+import Image from 'next/image'
 
 interface CardProps {
   title: string
-  content: ReactNode
+  imageUrl: string
   subTitles: string[]
   color?: string
 }
-export default function Card({ title, content, subTitles, color }: CardProps) {
+export default function Card({ title, imageUrl, subTitles, color }: CardProps) {
   return (
-    <div className={`flex flex-col w-[280px] h-[200px] shadow-shadow1 rounded-[20px] ${color}`}>
-      <div className="relative w-full h-full overflow-hidden pt-[50px] pb-[10px] px-[15px]">
-        <div className="absolute -right-16 -top-11">
-          <Pokeball width={200} fill={theme.color.grayscale.Background} opacity={0.2} />
+    <div className={`flex flex-col w-[184px] h-[103px] shadow-shadow1 rounded-[12px] ${color}`}>
+      <div className="relative w-full h-full overflow-hidden pt-[10px] pb-[10px] px-[15px]">
+        <div className="absolute -right-10 -top-8">
+          <Pokeball width={120} fill={theme.color.grayscale.Background} opacity={0.2} />
         </div>
-        <div className="text-headline text-white">{title}</div>
+        <div className="flex flex-col">
+          <div className="text-caption text-white">#123</div>
+          <div className="text-heading3 text-white">{title}</div>
+        </div>
         <div className="flex flex-col gap-[10px]">
           {subTitles.map((sub) => (
             <div
               key={sub}
-              className="text-subtitle1 w-fit font-medium text-white p-[8px] px-[20px] rounded-[20px]"
+              className="text-body w-fit text-white p-[2px] px-[13px] rounded-[15px]"
               style={{ backgroundColor: 'rgba(255, 255, 255, 0.2)' }}
             >
               {sub}
             </div>
           ))}
         </div>
-        <div className="absolute bottom-0 right-[10px]">{content}</div>
+        <div className="absolute bottom-[10px] right-[5px]">
+          <Image src={imageUrl} width={85} height={100} alt={'pokemon'} />
+        </div>
       </div>
     </div>
   )

fe60d5aa89d09d72cb47ea0ab69e3894ea57f6b3
김솔지 <so1gging@gmail.com>
2023-12-13 16:19:52 +0900

    :necktie: revert

--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,16 @@
 import HomeTemplate from '@/templates/pages/home/HomeTemplate'
 import Pokeball from '@/assets/icons/pokeball.svg'
 import theme from '@/libs/styles/theme'
-import { QueryClient, QueryClientProvider } from 'react-query'
-
-const queryClient = new QueryClient()
 
 export default function Home() {
   return (
     <main>
-      <QueryClientProvider client={queryClient}>
-        <div className="relative w-screen h-screen overflow-hidden">
-          <div className="absolute -right-16 -top-11">
-            <Pokeball width={200} fill={theme.color.grayscale.Background} />
-          </div>
-          <HomeTemplate />
+      <div className="relative w-screen h-screen overflow-hidden">
+        <div className="absolute -right-16 -top-11">
+          <Pokeball width={200} fill={theme.color.grayscale.Background} />
         </div>
-      </QueryClientProvider>
+        <HomeTemplate />
+      </div>
     </main>
   )
 }

46f73dac3de642c5e65360826ffa10147273c556
김솔지 <so1gging@gmail.com>
2023-12-13 16:13:04 +0900

    :lipstick: HomePage 수정

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,11 @@
 import HomeTemplate from '@/templates/pages/home/HomeTemplate'
 import Pokeball from '@/assets/icons/pokeball.svg'
 import theme from '@/libs/styles/theme'
-import { useRouter } from 'next/navigation'
 
 export default function Home() {
-  const router = useRouter()
-
-  const handleLogoClick = () => {
-    router.push('/')
-  }
-
   return (
     <main>
-      <div className="relative w-screen h-screen overflow-hidden" onClick={handleLogoClick}>
+      <div className="relative w-screen h-screen overflow-hidden">
         <div className="absolute -right-16 -top-11">
           <Pokeball width={200} fill={theme.color.grayscale.Background} />
         </div>
